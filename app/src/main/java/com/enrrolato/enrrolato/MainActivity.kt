dce4fc1d1c01c206a36b5143e3396be9d1403084
BranDaniMB <brandanimb@gmail.com>
2020-05-27 14:02:21 -0600

    login with Google

--- app/src/main/java/com/enrrolato/enrrolato/MainActivity.kt
+++ app/src/main/java/com/enrrolato/enrrolato/MainActivity.kt
@@ -6,11 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +39,31 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
 
         loginLayout.visibility = View.VISIBLE
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showMenu(account.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                showAlert();
+                            }
+                        }
+                }
+            } catch (e: ApiException) {
+                showAlert()
+            }
+        }
     }
 
     private fun session() {
@@ -59,6 +91,18 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+
+        // Google login
+        googleLoginBtn.setOnClickListener{
+            val googleConfig = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleClient = GoogleSignIn.getClient(this, googleConfig)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
         }
 
         // Para crear la cuenta, da error si ya esta creada.

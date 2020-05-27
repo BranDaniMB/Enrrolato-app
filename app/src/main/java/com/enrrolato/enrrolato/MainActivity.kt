48d2a9e403acb3b17690d026253d52d86cff1736
BranDaniMB <brandanimb@gmail.com>
2020-05-27 12:50:08 -0600

    add email/password login

--- app/src/main/java/com/enrrolato/enrrolato/MainActivity.kt
+++ app/src/main/java/com/enrrolato/enrrolato/MainActivity.kt
@@ -3,6 +3,9 @@ package com.enrrolato.enrrolato
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,69 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Analytics Event
+        val analytics : FirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        val bundle = Bundle()
+        bundle.putString("message", "Pantalla de inicio de sesión lanzada")
+        analytics.logEvent("LoginScreen", bundle)
+
+        // Setup
+        setup()
+    }
+
+    private fun setup() {
+
+        // Da error si la cuenta no esta registrada
+        loginBtn.setOnClickListener {
+            if (usernameField.text.isNullOrEmpty() && passwordField.text.isNullOrEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(usernameField.text.toString()
+                    , passwordField.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showMenu(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert();
+                    }
+                }
+            }
+        }
+
+        // Para crear la cuenta, da error si ya esta creada.
+        /*loginBtn.setOnClickListener {
+            if (usernameField.text.isNullOrEmpty() && passwordField.text.isNullOrEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(usernameField.text.toString()
+                    , passwordField.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showMenu(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert();
+                    }
+                }
+            }
+        }*/
+
+        // Para cerrar sesión
+        /*loginBtn.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }*/
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando el usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog =  builder.create();
+        dialog.show();
+    }
+
+    private fun showMenu(email: String, provider: ProviderType) {
+        val menuIntent = Intent(this, MenuScreen::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider)
+        }
+        startActivity(menuIntent)
     }
 }
 
--- app/src/main/java/com/enrrolato/enrrolato/MenuScreen.kt
+++ app/src/main/java/com/enrrolato/enrrolato/MenuScreen.kt
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 
+enum class ProviderType {
+    BASIC
+}
+
 class MenuScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_screen)
 
-        //window.statusBarColor = Color.parseColor("@colors/backgroundColor")
-       //getWindow().setNavigationBarColor(Color.parseColor("@colors/backgroundColor")
+        // setup
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup();
+    }
+
+    private fun setup() {
 
     }
 }

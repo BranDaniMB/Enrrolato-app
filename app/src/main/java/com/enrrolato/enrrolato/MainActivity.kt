1be384859f836cb6a6051da6a3b211007019bf90
ma-sc-24 <msmasaca@gmail.com>
2020-05-31 20:40:56 -0600

    31052020 - Comunicación entre las ventanas de perfil y acerca de y viceversa - Links a FB e IG listos (nada más hay que cambiar el de FB porque lo hice de prueba con uno)

--- app/src/main/java/com/enrrolato/enrrolato/MainActivity.kt
+++ app/src/main/java/com/enrrolato/enrrolato/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.enrrolato.enrrolato
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_about_us.*
+import kotlinx.android.synthetic.main.fragment_profile_screen.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val manager = supportFragmentManager
+
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+
     }
+
 

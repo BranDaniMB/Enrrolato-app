c5af6ca734afbcfc2df77d1cd1e072712fc00a81
ma-sc-24 <msmasaca@gmail.com>
2020-06-10 21:44:47 -0600

    10062020 -Pupop de cerrar sesión

--- app/src/main/java/com/enrrolato/enrrolato/ProfileScreenFragment.kt
+++ app/src/main/java/com/enrrolato/enrrolato/ProfileScreenFragment.kt
@@ -8,11 +8,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.fragment_profile_screen.*
-
-import com.enrrolato.enrrolato.database.Enrrolato
 
 /**
  * A simple [Fragment] subclass.
@@ -26,7 +24,7 @@ import com.enrrolato.enrrolato.database.Enrrolato
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile_screen, container, false)
         val btAbout = view.findViewById<View>(R.id.btAboutUs) as Button
-        val btExit = view.findViewById<View>(R.id.btLogout) as Button
+        val btOut = view.findViewById<View>(R.id.btLogout) as Button
         val btRestore = view.findViewById<View>(R.id.btChangePassword) as Button
         showEmail(view)
 
@@ -38,7 +36,7 @@ import com.enrrolato.enrrolato.database.Enrrolato
             showRestoreFragment()
         }
 
-        btExit.setOnClickListener {
+        btOut.setOnClickListener {
             logOut()
         }
 
@@ -73,10 +71,53 @@ import com.enrrolato.enrrolato.database.Enrrolato
     }
 
     private fun logOut() {
-        val int: Intent = Intent(activity, MainActivity::class.java)
-        startActivity(int)
-        Toast.makeText(context, "Usted ha cerrado sesión", Toast.LENGTH_SHORT).show()
+        val alertDialogBuilder = context?.let { AlertDialog.Builder(it, R.style.alert_dialog) }
+        val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+        val popupConfirmExitView = layoutInflater.inflate(R.layout.activity_popup_confirm_exit, null)
+        val bt_ok: Button = popupConfirmExitView.findViewById(R.id.btOk);
+        val bt_cancel: Button = popupConfirmExitView.findViewById(R.id.btCancel);
+        alertDialogBuilder?.setView(popupConfirmExitView)
+        val alertDialog: AlertDialog = alertDialogBuilder!!.create()
+        alertDialog.window?.attributes!!.windowAnimations = R.style.alert_dialog
+        alertDialog.show()
 
+        bt_ok.setOnClickListener {
+            fragmentManager!!.beginTransaction()!!.remove(this).commit();
+            alertDialog.cancel()
+
+            val int: Intent = Intent(activity, MainActivity::class.java)
+            startActivity(int)
+            Toast.makeText(context, "Usted ha cerrado sesión", Toast.LENGTH_SHORT).show()
+        }
+
+        bt_cancel.setOnClickListener {
+            alertDialog.cancel()
+        }
+/*
+*  LayoutInflater layoutInflater = LayoutInflater.from(ExamActivity.this);
+            View popupInputDialogView = layoutInflater.inflate(R.layout.popup_exit_exam, null);
+            Button bt_save = popupInputDialogView.findViewById(R.id.bt_save);
+            Button bt_cancel = popupInputDialogView.findViewById(R.id.bt_cancel);
+            alertDialogBuilder.setView(popupInputDialogView);
+            final AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.getWindow().getAttributes().windowAnimations = R.style.alert_dialog;
+            alertDialog.show();
+            bt_save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ExamActivity.this.finish();
+                    alertDialog.cancel();
+                }
+            });
+            bt_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    alertDialog.cancel();
+                }
+            });
+* */
+
+    }
 /*        if (logOut == true) {
             FirebaseAuth.getInstance().signOut()
             val i2 = requireActivity().Intent(this@ProfileScreenFragment, MainActivity::class.java)
@@ -87,7 +128,5 @@ import com.enrrolato.enrrolato.database.Enrrolato
 //        val i1 = Intent(activity, MainActivity::class.java)
 //        startActivity(i1)
 //        Toast.makeText(ProfileScreenFragment.this, "Usted ha cerrado sesión", Toast.LENGTH_SHORT).show()
-    }
-
 
 }

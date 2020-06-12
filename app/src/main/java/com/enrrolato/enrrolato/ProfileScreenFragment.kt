9f2ddf20ce703020b834bd9d7707a11555542dc2
BranDaniMB <brandanimb@gmail.com>
2020-06-12 16:57:07 -0600

    commit

--- app/src/main/java/com/enrrolato/enrrolato/ProfileScreenFragment.kt
+++ app/src/main/java/com/enrrolato/enrrolato/ProfileScreenFragment.kt
@@ -17,9 +17,13 @@ import com.google.firebase.auth.FirebaseAuth
  * A simple [Fragment] subclass.
  */
 
-  class ProfileScreenFragment : Fragment() {
+class ProfileScreenFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile_screen, container, false)
         val btAbout = view.findViewById<View>(R.id.btAboutUs) as Button
@@ -72,7 +76,8 @@ import com.google.firebase.auth.FirebaseAuth
     private fun logOut() {
         val alertDialogBuilder = context?.let { AlertDialog.Builder(it, R.style.alert_dialog) }
         val layoutInflater: LayoutInflater = LayoutInflater.from(context)
-        val popupConfirmExitView = layoutInflater.inflate(R.layout.activity_popup_confirm_exit, null)
+        val popupConfirmExitView =
+            layoutInflater.inflate(R.layout.activity_popup_confirm_exit, null)
         val bt_ok: Button = popupConfirmExitView.findViewById(R.id.btOk);
         val bt_cancel: Button = popupConfirmExitView.findViewById(R.id.btCancel);
         alertDialogBuilder?.setView(popupConfirmExitView)
@@ -87,45 +92,13 @@ import com.google.firebase.auth.FirebaseAuth
             val int: Intent = Intent(activity, MainActivity::class.java)
             startActivity(int)
             Toast.makeText(context, "Usted ha cerrado sesión", Toast.LENGTH_SHORT).show()
+            val prefs = activity!!.getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
         }
 
         bt_cancel.setOnClickListener {
             alertDialog.cancel()
         }
-/*
-*  LayoutInflater layoutInflater = LayoutInflater.from(ExamActivity.this);
-            View popupInputDialogView = layoutInflater.inflate(R.layout.popup_exit_exam, null);
-            Button bt_save = popupInputDialogView.findViewById(R.id.bt_save);
-            Button bt_cancel = popupInputDialogView.findViewById(R.id.bt_cancel);
-            alertDialogBuilder.setView(popupInputDialogView);
-            final AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.getWindow().getAttributes().windowAnimations = R.style.alert_dialog;
-            alertDialog.show();
-            bt_save.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ExamActivity.this.finish();
-                    alertDialog.cancel();
-                }
-            });
-            bt_cancel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    alertDialog.cancel();
-                }
-            });
-* */
-
     }
-/*        if (logOut == true) {
-            FirebaseAuth.getInstance().signOut()
-            val i2 = requireActivity().Intent(this@ProfileScreenFragment, MainActivity::class.java)
-            startActivity(i2)
-            finish()
-            return true */
-
-//        val i1 = Intent(activity, MainActivity::class.java)
-//        startActivity(i1)
-//        Toast.makeText(ProfileScreenFragment.this, "Usted ha cerrado sesión", Toast.LENGTH_SHORT).show()
-
 }
